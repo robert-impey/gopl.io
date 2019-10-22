8e4951abdf5a698d34ffdac22832ed1095e16c22
Robert Impey <robert.impey@gmail.com>
2019-10-22 23:55:52 +0100

    C8: Allow to listen to other machines.

--- ch8/clock2/clock.go
+++ ch8/clock2/clock.go
@@ -25,7 +25,7 @@ func handleConn(c net.Conn) {
 }
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:8000")
+	listener, err := net.Listen("tcp", "0.0.0.0:8000")
 	if err != nil {
 		log.Fatal(err)
 	}
